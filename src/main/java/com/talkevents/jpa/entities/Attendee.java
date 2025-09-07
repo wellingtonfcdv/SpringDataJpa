4fa46f7495b693a816f3387d4bc8a0ae5e0f3bcd
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-09-07 12:56:16 -0300

    Criando entidade Attendee

---
+++ src/main/java/com/talkevents/jpa/entities/Attendee.java
@@ -0,0 +1,44 @@
+package com.talkevents.jpa.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+@Entity
+@Table(name = "Attendee")
+public class Attendee implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(length = 100)
+    private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
